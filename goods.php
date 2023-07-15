27c128dc510a5cc05d725a37395bf741e4c95636
Daniel <polzikd@yandex.ru>
2023-07-15 14:55:52 +0300

    add list of goods

---
+++ goods.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>List of Goods</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body class="loggedin">
+<nav class="navtop">
+    <div>
+        <a href="home.php" class="title"><h1>Website Title</h1></a>
+        <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+        <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+    </div>
+</nav>
+<div class="content">
+<?php
+// Database connection details
+$host = 'localhost';
+$username = 'root';
+$password = 'root';
+$database = 'example';
+
+// Create a new MySQLi object
+$mysqli = new mysqli($host, $username, $password, $database);
+
+// Check for connection errors
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit();
+}
+
+// Fetch the goods from the database
+$query = "SELECT id, name, description, image_link FROM goods";
+$result = $mysqli->query($query);
+
+// Display the goods
+if ($result->num_rows > 0) {
+    echo '<ul class="goods-list">';
+    while ($row = $result->fetch_assoc()) {
+        echo '<li class="goods-item">';
+        echo '<img src="' . $row['image_link'] . '" alt="Goods Image" class="goods-image">';
+        echo '<div>';
+        echo '<h3 class="goods-name">' . $row['name'] . '</h3>';
+        echo '<p>' . $row['description'] . '</p>';
+        echo '</div>';
+        echo '</li>';
+    }
+    echo '</ul>';
+} else {
+    echo 'No goods found.';
+}
+
+// Close the database connection
+$mysqli->close();
+?>
+
+</div>
+</body>
+</html>
