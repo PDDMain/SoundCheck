d270f76654e63717f1af29224a9dc59fbb6d88c1
Daniel <polzikd@yandex.ru>
2023-07-24 16:14:39 +0300

    fix add to basket

--- add_to_basket.php
+++ add_to_basket.php
@@ -10,46 +10,50 @@ if (!isset($_SESSION['loggedin'])) {
 // Check if the product ID is provided via POST
 if (isset($_POST['good_id']) && !empty($_POST['good_id'])) {
     // Database connection details
-    $host = 'localhost';
-    $username = 'root';
-    $password = 'root';
-    $database = 'example';
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = 'root';
+    $DATABASE_NAME = 'example';
 
-    // Create a new MySQLi object
-    $mysqli = new mysqli($host, $username, $password, $database);
+    // Try and connect using the info above.
+    $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
     // Check for connection errors
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: " . $mysqli->connect_error;
-        exit();
+    if (mysqli_connect_errno()) {
+        // If there is an error with the connection, stop the script and display the error.
+        exit('Failed to connect to MySQL: ' . mysqli_connect_error());
     }
 
     // Retrieve the user ID from the session
     $user_id = $_SESSION['id'];
 
     // Get the product ID from the POST data
-    $good_id = $mysqli->real_escape_string($_POST['good_id']);
+    $good_id = $con->real_escape_string($_POST['good_id']);
 
-    // Check if the product is already in the basket for the user
-    $check_query = "SELECT count FROM basket WHERE user_id = $user_id AND good_id = $good_id";
-    $check_result = $mysqli->query($check_query);
+    echo "user_id = `$user_id`, good_id = `$good_id`";
+    // Check if the product is already in the basket
+//    $check_query = "SELECT id FROM basket WHERE user_id = $user_id AND good_id = $good_id";
+    if ($stmt = $con->prepare("SELECT user_id, good_id FROM basket WHERE user_id = $user_id AND good_id = $good_id")) {
+        $stmt->execute();
+        $stmt->store_result();
 
-    if ($check_result->num_rows === 0) {
-        // If the product is not in the basket, insert it with count=1
-        $insert_query = "INSERT INTO basket (user_id, good_id, count) VALUES ($user_id, $good_id, 1)";
-        $mysqli->query($insert_query);
-    } else {
-        // If the product is already in the basket, increment its count
-        $row = $check_result->fetch_assoc();
-        $current_count = $row['count'];
-        $new_count = $current_count + 1;
+        if ($stmt->num_rows === 0) {
+            // Insert the product into the basket
+            $insert_query = "INSERT INTO basket (user_id, good_id) VALUES ($user_id, $good_id)";
+            echo "$insert_query";
+            if ($stmt = $con->prepare($insert_query)) {
+                echo "$insert_query";
+                $stmt->execute();
 
-        $update_query = "UPDATE basket SET count = $new_count WHERE user_id = $user_id AND good_id = $good_id";
-        $mysqli->query($update_query);
+                header('Location: goods.php');
+                exit;
+            }
+            $stmt->close();
+        }
     }
 
     // Close the database connection
-    $mysqli->close();
+    $con->close();
 }
 
 // Redirect back to the goods.php page after adding to the basket
