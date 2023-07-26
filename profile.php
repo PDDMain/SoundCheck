2ebeff98ce63ecbcaa93baa0d37f91d5788f1e96
Daniel <polzikd@yandex.ru>
2023-07-26 11:16:04 +0300

    add profile.php

--- profile.php
+++ profile.php
@@ -14,51 +14,171 @@ $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_
 if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get the updated values from the form
+    $action = $_POST["action"];
+    if ($action === 'logout') {
+        header('Location: php/logout.php');
+        exit();
+    }
+}
+
 // We don't have the password or email info stored in sessions, so instead, we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+$stmt = $con->prepare('SELECT username, password, first_name, second_name, email, phone FROM accounts WHERE id = ?');
 // In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
-$stmt->bind_result($password, $email);
+$stmt->bind_result($username, $password, $first_name, $second_name, $email, $phone);
 $stmt->fetch();
 $stmt->close();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Get the updated values from the form
+    $action = $_POST["action"];
+    if ($action === 'edit') {
+        $username_form = $_POST["username"];
+        $first_name_form = $_POST["first_name"];
+        $second_name_form = $_POST["second_name"];
+        $email_form = $_POST["email"];
+        $phone_form = $_POST["phone"];
+
+        if ($con->connect_error) {
+            die("Connection failed: " . $con->connect_error);
+        }
+
+        if (empty($username_form) || empty($first_name_form) || empty($second_name_form) || empty($email_form) || ($phone_form === 0)) {
+            echo "Error: Please fill in all required fields.";
+            exit;
+        }
+
+        if (($username !== $username_form) || ($first_name !== $first_name_form) ||
+            ($second_name !== $second_name_form) || ($email !== $email_form) || ($phone !== $phone_form)) {
+
+            if ($username !== $username_form) {
+                $stmt = $con->prepare("SELECT * FROM accounts WHERE username = ?");
+                $stmt->bind_param("s", $username_form);
+                $stmt->execute();
+                $result = $stmt->get_result();
+
+                if ($result->num_rows > 0) {
+                    echo "Error: The username is already taken.";
+                    $stmt->close();
+                    $con->close();
+                    exit;
+                }
+            }
+            $insert_stmt = $con->prepare("UPDATE accounts SET username = ?, first_name = ?, second_name = ?, email = ?, phone = ? WHERE id = ?");
+            $insert_stmt->bind_param("sssssi",$username_form, $first_name_form, $second_name_form, $email_form, $phone_form, $_SESSION['id']);
+            if ($insert_stmt->execute()) {
+                // Data inserted successfully
+                $username = $username_form;
+                $first_name = $first_name_form;
+                $second_name = $second_name_form;
+                $email = $email_form;
+                $phone = $phone_form;
+            } else {
+                // Error occurred while inserting data
+                echo "Error: " . $insert_stmt->error;
+                exit();
+            }
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Profile Page</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/user-style.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/header-cathegories.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
 </head>
-<body class="loggedin">
-<nav class="navtop">
-    <div>
-        <a class="title"><h1>Website Title</h1></a>
-        <a href="goods.php"><i class="fas fa-user-circle"></i>Goods</a>
-        <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-        <a href="php/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+<body>
+<header>
+    <div class="header-navigation">
+        <div><a href="main.php"><img class="logo" src="img/logo.jpg"></a></div>
+        <div class="search-container">
+            <div class="search">
+                <input type="text" placeholder="Search">
+                <button class="search-button">Search</button>
+            </div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="profile.php">
+                        <div class="user-container">
+                            <img class="user" src="img/user_icon.jpg">
+                            <img class="user-hover" src="img/user_icon_hover.jpg">
+                        </div>
+                    </a></li>
+                <li><a href="basket.php">
+                        <div class="cart-container">
+                            <img class="cart" src="img/shopping_cart_icon.jpg">
+                            <img class="cart-hover" src="img/shopping_cart_icon_hover.jpg">
+                        </div>
+                    </a></li>
+            </ul>
+        </nav>
     </div>
-</nav>
-<div class="content">
-    <h2>Profile Page</h2>
-    <div>
-        <p>Your account details are below:</p>
-        <table>
-            <tr>
-                <td>Username:</td>
-                <td><?= $_SESSION['name'] ?></td>
-            </tr>
-            <tr>
-                <td>Password:</td>
-                <td><?= $password ?></td>
-            </tr>
-            <tr>
-                <td>Email:</td>
-                <td><?= $email ?></td>
-            </tr>
-        </table>
+    <div class="header-cathegories">
+        <a class="cath" href="#">In-ear</a>
+        <a class="cath" href="#">Over-ear</a>
+        <a class="cath" href="#">Wireless</a>
     </div>
-</div>
+</header>
+<div></div>
+<main>
+    <div class="user-page">
+        <div class="user-info">
+            <form class="us-inf" method="post">
+                <div class="my-info">My information</div>
+                <div class="information-title">First name</div>
+                <input class="information" contenteditable="true" type="text" name="first_name"
+                       value="<?php echo $first_name; ?>">
+                <!--        <hr>-->
+                <div class="information-title">Last name</div>
+                <input class="information" contenteditable="true" type="text" name="second_name"
+                       value="<?php echo $second_name; ?>">
+                <!--        <hr>-->
+                <div class="information-title">Username</div>
+                <input class="information" contenteditable="true" type="text" name="username"
+                       value="<?php echo $username; ?>">
+                <!--        <hr>-->
+                <div class="information-title">Email</div>
+                <input class="information" contenteditable="true" type="text" name="email"
+                       value="<?php echo $email; ?>">
+                <!--        <hr>-->
+                <div class="information-title">Phone number</div>
+                <input class="information" contenteditable="true" type="text" name="phone"
+                       value="<?php echo $phone; ?>">
+                <button type="submit" name="action" value="edit" class="edit">Confirm changes</button>
+                <button name="action" value="logout" class="logout">Log out</button>
+            </form>
+        </div>
+
+        <div class="orders-info">
+            <div class="orders-title">Order history</div>
+            <div class="order-info">
+                <div class="order-id">Order ID: 123456-7938</div>
+                <div class="order-date">Date: 12.03.2023</div>
+                <div class="order-price"> 12579$</div>
+            </div>
+            <hr>
+            <div class="order-info">
+                <div class="order-id">Order ID: 123456-7938</div>
+                <div class="order-date">Date: 12.03.2023</div>
+                <div class="order-price"> 10$</div>
+            </div>
+        </div>
+    </div>
+</main>
+<footer class="footer">
+    <p>&copy; 2023 All rights reserved.</p>
+</footer>
 </body>
 </html>
